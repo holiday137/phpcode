ea1f287e9e0bb67903c6a5d689ae966b6eb14937
tujiayi <1976717845@qq.com>
2016-10-06 23:26:39 +0800

    23:26

--- index.php
+++ index.php
@@ -12,11 +12,17 @@
 	 echo $code;
 $url='https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4c3f6253ec83cade&secret=2bc029f826329a801d840b1fd5b88dbd&code='.$code.
 	'&grant_type=authorization_code ';
-$html = file_get_contents($url);  
-echo $html;  
-$arr=json_decode($html,true);
-echo $arr['access_token'];
-	echo '欢迎光临'
+$getaccess = file_get_contents($url);  
+$arr=json_decode($getaccess,true);
+$access_token=$arr['access_token'];
+$openid=$arr['openid'];
+
+$url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
+$getinfo = file_get_contents($url); 
+$userinfo=json_decode($getinfo,true);
+print_r($userinfo);
+
+	echo '<br />'.'欢迎光临';
 ?>
 
 </body>
