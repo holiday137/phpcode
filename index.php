166e03b391197532bac0808d6e90d43d28b03450
tujiayi <1976717845@qq.com>
2016-10-06 23:07:38 +0800

    23:07

--- index.php
+++ index.php
@@ -8,6 +8,14 @@
 <?php  
 	 // $wechatObj = new wechatCallbackapiTest();
 	 // $wechatObj->valid();
+	$code=$_GET['code'];
+	 echo $code;
+$url='https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx4c3f6253ec83cade&secret=2bc029f826329a801d840b1fd5b88dbd&code='.$code.
+	'&grant_type=authorization_code ';
+$html = file_get_contents($url);  
+echo $html;  
+$arr=json_decode($html,true);
+echo $arr['access_token'];
 	echo '欢迎光临'
 ?>
 
